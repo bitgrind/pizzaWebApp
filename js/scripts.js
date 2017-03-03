afd24fc74683320c3e4da74d52765db9762facd9
bitgrind <keithstedman@gmail.com>
2017-03-03 10:02:27 -0800

    toppings for cheese array, meat array and veggie array created along with images that correspond with each

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 //Business Logic
-function Pizza(toppings,size,price){
+function pizza(toppings,size,price){
   this.toppings = toppings;
   this.size = size;
   this.price = price;
 }
 
-function Order(delieveryMethod, pizza){
+function order(delieveryMethod, pizza){
   this.delieveryType = delieveryMethod;
   this.pizza = delieveryMethod;
 }
 
-Pizza.prototype.cost = function(pizza){
+pizza.prototype.cost = function(pizza){
   this.pizza = pizza;
 
   var cost;
@@ -18,7 +18,7 @@ Pizza.prototype.cost = function(pizza){
   return cost;
 }
 
-Pizza.prototype.waitTime = function(order){
+pizza.prototype.waitTime = function(order){
   this.pizza = pizza;
 
   var waitTime;
@@ -27,14 +27,17 @@ Pizza.prototype.waitTime = function(order){
 }
 
 //Varibles
-var newOrder = new Order();
-var newPizza = new Pizza();
+var newOrder = new order();
+var newPizza = new pizza();
 var delieveryType;
 var pizzaSize;
 var pizzaTopping = [];
+var cheeseToppings = ["cheddar","mozzarella","provolone","parmesan"]
+var meatToppings = ["cheese","perperoni","canadianBacon","bacon","chicken"]
+var veggieToppings = ["olives","greenOlives","greenPeppers","redPeppers","jalapenos","greenOnion","redOnion","mushrooms","pepperoncini","tomatoes","pineapple"]
 
 
 //User Logic
 $(function(){
-  console.log('js is a go');
+
 });
