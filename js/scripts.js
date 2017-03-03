e9582ebaf2dd80ba6a8dcf2308edd6fc4adedf06
bitgrind <keithstedman@gmail.com>
2017-03-03 09:27:04 -0800

    pizza object, ordering object, cost prototype and waitTime prototype with basic varibles and return values in place

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,36 @@ function Pizza(toppings,size,price){
   this.price = price;
 }
 
-Pizza.prototype.cost(pizza){
-  this.pizza = pizza;
+function Order(delieveryMethod, pizza){
+  this.delieveryType = delieveryMethod;
+  this.pizza = delieveryMethod;
 }
 
-Pizza.prototype.waitTime(pizza){
+Pizza.prototype.cost = function(pizza){
   this.pizza = pizza;
+
+  var cost;
+
+  return cost;
 }
+
+Pizza.prototype.waitTime = function(order){
+  this.pizza = pizza;
+
+  var waitTime;
+
+  return waitTime;
+}
+
+//Varibles
+var newOrder = new Order();
+var newPizza = new Pizza();
+var delieveryType;
+var pizzaSize;
+var pizzaTopping = [];
 
 
 //User Logic
 $(function(){
-
+  console.log('js is a go');
 });
