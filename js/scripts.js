d31a8ce29f92b88c998a766d76451b75f7d03055
bitgrind <keithstedman@gmail.com>
2017-03-03 10:40:38 -0800

    added vars to prototype cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
 //Business Logic
-function pizza(toppings,size,price){
+function Pizza(toppings,size){
   this.toppings = toppings;
   this.size = size;
-  this.price = price;
 }
 
-function order(delieveryMethod, pizza){
-  this.delieveryType = delieveryMethod;
-  this.pizza = delieveryMethod;
-}
-
-pizza.prototype.cost = function(pizza){
+function Order(customer, delievery, pizza){
+  this.customer = customer;
+  this.delieveryType = delievery;
   this.pizza = pizza;
+}
 
+Pizza.prototype.cost = function(order){
+  var order = order;
+  var customer = order.customer;
+  var delievery = order.delievery;
+  var pizza = order.pizza;
+  var pizzaSize = order.pizza.size[1];
+  var pizzaToppings = order.pizza.toppings;
   var cost;
+
+  pizzaToppings.forEach(function(){
+    cost
+  });
+
+  console.log(pizzaToppings);
+
 
   return cost;
 }
 
-pizza.prototype.waitTime = function(order){
-  this.pizza = pizza;
-
+Pizza.prototype.waitTime = function(order){
+  var order = order;
   var waitTime;
 
   return waitTime;
@@ -29,16 +39,19 @@ pizza.prototype.waitTime = function(order){
 //Varibles
 //Static
 var pizzaSize = [["personal", "8 inch"],["medium", "12 inch"],["large", "16 inch"],["party", "20 inch"]];
+var delieveryType = ["pickup","delievery","in-store"];
 var cheeseToppings = ["cheddar","mozzarella","provolone","parmesan"]
 var meatToppings = ["cheese","perperoni","canadianBacon","bacon","chicken"]
-var veggieToppings = ["olives","greenOlives","greenPeppers","redPeppers","jalapenos","greenOnion","redOnion","mushrooms","pepperoncini","tomatoes","pineapple"]
+var veggieToppings = ["blackOlives","greenOlives","greenPeppers","redPeppers","jalapenos","greenOnion","redOnion","mushrooms","pepperoncini","tomatoes","pineapple"]
 
 //Dynamtic
-var newPizzaOrder = new order();
-var newPizza = new pizza();
+var newCustomer = "Keith";
+//arguments: toppings,size,price
+var newPizza = new Pizza([["cheddar"],["sausage"],["greenOlives","blackOlives"]],pizzaSize[1]);
+//arguments: customer, delievery, pizza
+var newOrder = new Order("Keith",delieveryType[0],newPizza);
 var newPizzaDelieveryType;
 var newPizzaSize;
-var newPizzaTopping = [];
 
 //User Logic
 $(function(){
